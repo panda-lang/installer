9f3b53f975f36a7b79782d20596fc81e7f03b898
Kasper Seweryn <casper@wvffle.net>
2019-10-20 04:58:40 +0200

    License view concept

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,47 @@
   .row
     .col.has-logo
       img.logo(src='@/assets/images/installer.png')
-    .col
-      v-stepper(:steps='steps', v-model='step')
+    .col.main-frame
+      v-stepper(:steps='steps', v-model='step', ref='stepper')
+
+      .content
+        template(v-if='step === 1')
+          h1 License agreement
+          p Panda is licensed under Apache License 2.0
+          template(v-if='showFullLicense')
+            pre.full-license
+              include ../public/panda_license.txt
+          template(v-else)
+            .row
+              ul.col
+                h3 Permissions
+                li Commercial use
+                li Modification
+                li Distribution
+                li Patent use
+                li Private use
+              ul.col
+                h3 Restrictions
+                li Trademark use
+                li Liability
+                li Warranty
+              ul.col
+                h3 Conditions
+                li License and copyright notice
+                li State changes
+          .row.license-row
+            .col
+              a.pre(@click='showFullLicense = !showFullLicense')
+                | {{ showFullLicense ? 'Hide full license' : 'Read full License' }}
+            .col
+              .row
+                .col
+                  input#license(type='checkbox', v-model='licenseAgreed')
+                  label(for='license') #[span(:class='{ checked: licenseAgreed }') {{ licenseAgreed ? '☑' : '☐' }}] I agree to the license
+          .text-center
+            .btn(:disabled='!licenseAgreed', @click='licenseAgreed && $refs.stepper.next()') Next
+        template(v-if='step === 2') 2
+        template(v-if='step === 3') 3
 </template>
 
 <script>
@@ -19,7 +58,9 @@ export default {
   data () {
     return {
       steps: 3,
-      step: undefined
+      step: undefined,
+      licenseAgreed: false,
+      showFullLicense: false
     }
   }
 }
